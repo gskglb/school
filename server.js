4f815d362c8d5925f951746781ef0a458b5db6d3
Guru <gskglb@gmail.com>
2018-08-22 23:11:50 +0530

    server added for heroku

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var serveStatic = require('serve-static')
 var app = express()
 var path = require('path')
 
-app.use(serveStatic(path.join(__dirname, '/dist/')))
+app.use(serveStatic(path.join(__dirname, '/dist')))
 
-var server = app.listen(process.env.PORT || 8080, function() {
+var server = app.listen(process.env.PORT || 5000, function() {
   var port = server.address().port
   console.log('App now running on port', port)
 })
